031b2d36832dfbc7f6997a49a288881220d755fb
callum <callumgibbons@icloud.com>
2024-04-17 17:37:03 +0530

    created all necessary files and made data to be used for each counter with randomly generated names and positions.

---
+++ src/components/WorkerData/WorkerData.tsx
@@ -0,0 +1,8 @@
+const workers = [
+  { id: 1, name: "Bette Cisneros", position: "Stockbroker" },
+  { id: 2, name: "Patsy Werner", position: "Art dealer" },
+  { id: 3, name: "Efrain Montgomery", position: "Surveyor" },
+  { id: 4, name: "Wanda Arroyo", position: "Editor" },
+  { id: 5, name: "Rex Faulkner", position: "Nurse" },
+  { id: 6, name: "Theodore Mccann", position: "Archivist" },
+];
